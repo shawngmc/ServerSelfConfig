c9b08a73cc591897f9fe62bb2faf400bd024b55f
Shawn McNaughton <shawngmc@gmail.com>
2015-05-02 21:48:48 -0400

    Initial implementation with server contact and lockout

--- CatFeedingTracker/ButtonManager.py
+++ CatFeedingTracker/ButtonManager.py
@@ -1,12 +1,32 @@
 import RPi.GPIO as GPIO
 import time
+import urllib2
 
+lockout_period_ms = 10000
+contact_url = "http://192.168.1.222:8080/input/WwL2MDpy2vhX8jdY6NgLfrrMY1B?private_key=0ONjdgZEjAuLMQ2wxaWjTjjWRGE&wasfed=Y"
+
+
+#####################################################
+
+# Set up the pins
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+last_press_time = 0
 
 while True:
 	input_state = GPIO.input(18)
 	if input_state == False:
-		print('Button Pressed')
+		current_time = getCurrentTimeMillis()
+		if (last_press_time + lockout_period_ms < current_time):
+			last_press_time = current_time
+			sendNotification()
 		time.sleep(0.2)
+
+		
+def sendNotification():
+	urllib2.urlopen(contact_url)
+	
+	
+def getCurrentTimeMillis():
+	return int(round(time.time() * 1000))
